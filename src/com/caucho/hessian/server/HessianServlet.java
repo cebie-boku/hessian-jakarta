e110e27cc21f50caf54742048daf6516d6b5c925
Caleb Ebie <caleb.ebie@boku.com>
2025-08-18 16:20:44 +0200

    DEVPLAT-1041: Migrate to jarkarta

--- src/com/caucho/hessian/server/HessianServlet.java
+++ src/com/caucho/hessian/server/HessianServlet.java
@@ -55,15 +55,14 @@ import java.io.PrintWriter;
 import java.io.Writer;
 import java.util.logging.Logger;
 
-import javax.servlet.GenericServlet;
-import javax.servlet.Servlet;
-import javax.servlet.ServletConfig;
-import javax.servlet.ServletException;
-import javax.servlet.ServletRequest;
-import javax.servlet.ServletResponse;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import jakarta.servlet.Servlet;
+import jakarta.servlet.ServletConfig;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletRequest;
+import jakarta.servlet.ServletResponse;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 
 import com.caucho.hessian.io.Hessian2Input;
 import com.caucho.hessian.io.SerializerFactory;
